b0dacbd36980cbe27d5a55b0f5428a0896ba4c6e
TANMAY SHRIKRISHNA JOSHI <166515094+tanmayjoshi01@users.noreply.github.com>
2026-07-06 19:50:57 +0530

    Add input.txt file to the repository

---
+++ ADDOFTWONUM.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter two numbers";
+    cin>>a>>b;
+
+    c=a+b;
+
+    cout<<"Addition is"<<c;
+    return 0;
+    
+}1
+
