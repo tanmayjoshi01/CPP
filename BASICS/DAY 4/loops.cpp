58d1a4809b998946f90717f014d7aecf2ced0f0a
TANMAY SHRIKRISHNA JOSHI <166515094+tanmayjoshi01@users.noreply.github.com>
2026-07-11 22:41:20 +0530

    while

---
+++ BASICS/DAY 4/loops.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   int n,i=1;
+   
+   cout<<"enetr n";
+    cin>>n;
+
+    while(i<=n)
+    {
+        cout<<i<<" ";
+        i++;
+    }
+    return 0;
+}
