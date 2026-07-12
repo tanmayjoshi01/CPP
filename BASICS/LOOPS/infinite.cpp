14c1f27f2175ac059a9d5d42d8e9a3fae4a2f82f
TANMAY SHRIKRISHNA JOSHI <166515094+tanmayjoshi01@users.noreply.github.com>
2026-07-12 12:49:17 +0530

    change '

--- BASICS/LOOPS/infinite.cpp
+++ BASICS/LOOPS/infinite.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-
+//To stop the loop
 int main()
 {
     int i = 0;
@@ -10,4 +10,13 @@ int main()
         cout<<"infinite loop";
     }
 return 0;
-}
+
+//for loop
+int main()
+{
+   
+    while(1)
+    {
+        cout<<"infinite loop
+}return 0;
+    }
