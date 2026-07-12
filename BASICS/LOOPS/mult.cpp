09177266a4adcdc911c6a42cce8c1548f630817c
TANMAY SHRIKRISHNA JOSHI <166515094+tanmayjoshi01@users.noreply.github.com>
2026-07-12 13:00:13 +0530

    multiplication

---
+++ BASICS/LOOPS/mult.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n, i;
+
+    cout << "Enter number: ";
+    cin >> n;
+
+    for(i = 1; i <= 10; i++)
+    {
+        cout << n << " * " << i << " = " << n * i << endl;
+    }
+
+    return 0;
+}
