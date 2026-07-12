2fd1e320a5532682aaa8fbe4c8be5ad436dec7bc
TANMAY SHRIKRISHNA JOSHI <166515094+tanmayjoshi01@users.noreply.github.com>
2026-07-12 14:34:07 +0530

    sumofn

---
+++ BASICS/LOOPS/sumofn.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int n;
+int sum=0;
+
+cout<<"enter n";
+cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        sum=sum+i;
+    }
+
+ cout<<sum;
+ 
+}
